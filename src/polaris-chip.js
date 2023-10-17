cd2b58201dfa9b1108067147251d06f834295455
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-17 13:04:07 -0400

    Aiden 1st commit

--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -1,3 +1,25 @@
 import { PolarisChip } from './PolarisChip.js';
 
-customElements.define('polaris-chip', PolarisChip);
+export class PolarisChip extends LitElement {
+    static get properties() {
+      return {
+        month: { type: String },
+        day: { type: String },
+        active: { type: Boolean, reflect: true}
+      };
+    }
+  
+    static get styles() {
+      return css`
+      `;
+    }
+  
+    constructor() {
+        super();
+        this.month = 'Oct';
+        this.day = "14";
+      }
+  
+    render() {}
+    }
+  

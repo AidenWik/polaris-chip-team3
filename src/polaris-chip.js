188156826b38d76c97ce7df0f04c080df9067d6e
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-17 13:13:25 -0400

    aa

--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -11,6 +11,34 @@ export class PolarisChip extends LitElement {
   
     static get styles() {
       return css`
+      .card-date .post-date {
+    margin-right: 10px;
+    text-align: center;
+    float: left;
+}
+
+.card-date .day {
+    background: #f7f7f;
+    border-radius: 2px 2px 0 0;
+    color: #444;
+    display: block;
+    font-size: 18px;
+    font-weight: 900;
+    padding: 10px 20px;
+}
+
+. .post-date .month {
+    display: block;
+    background: #1E407C;
+    border-radius: 0 0 2px 2px;
+    color: #FFF;
+    font-size: 0.8em;
+    font-weight: bold;
+    line-height: 1.8;
+    padding: 1px 10px;
+    text-transform: uppercase;
+}
+
       `;
     }
   

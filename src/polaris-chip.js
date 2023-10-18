fb713ccc23f93084132170b24a4ea04ae2e21ee7
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-18 16:36:35 -0400

    a

--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -13,12 +13,24 @@ export class PolarisChip extends LitElement {
         :host {
           color: white;
         }
+        .card-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+          background-color: #f5f5f5;
+          border-radius: 5px;
+          padding: 20px;
+        }
         .top-title .h2 {
           font-size: 25em;
           font-weight: 700;
           line-height: 3.6875rem;
           margin: 0 0 5px 0;
         }
+
       `;
     }
   

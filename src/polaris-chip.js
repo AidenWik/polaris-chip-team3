94266026e4787555bb137118bfea7a3dbd2f561d
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-22 16:12:51 -0400

    a

--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -24,7 +24,7 @@ class PolarisChip extends LitElement {
       }
       .images {
         background-color: #005fa9;
-        height: 171px;
+        height: 160px;
       }
       .images img:hover {
         opacity: 0.7;
